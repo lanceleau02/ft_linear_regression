6d451b2ad497860026547dccfb844274c6acd5ca
lanceleau02 <laprieur@1E6.42angouleme.fr>
2025-03-17 16:45:07 +0100

    trying to implement using a matrix

--- train.py
+++ train.py
@@ -1,14 +1,27 @@
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv('data.csv')
 
+def test():
+	# Create a matrice X
+	X = np.hstack(())
+
+def viewData():
+	mileage = df['km'].tolist()
+	price = df['price'].tolist()
+	plt.scatter(mileage, price, color = 'b')
+	plt.show()
+
 def formulas():
-	alpha, m = 0.90, df.shape[0]
+	tmpTheta0, tmpTheta1, alpha, m = 0, 0, 0.01, df.shape[0]
 	
-	for index, row in df.iterrows():
-		tmpTheta0 = alpha * (1 / m) * sum(0, (m - 1)) * (tmpTheta0 + (tmpTheta1 * row['km']), row['price'])
-		tmpTheta1 = alpha * (1 / m) * sum(0, (m - 1)) * (tmpTheta0 + (tmpTheta1 * row['km']), row['price']) * row['km']
+	for i in range(0, 1000):
+		error = tmpTheta0 + (tmpTheta1 * row['km']) - row['price']
+		tmpTheta0 -= alpha * (1 / m) * sum(range(0, m)) * error
+		tmpTheta1 -= alpha * (1 / m) * sum(range(0, m)) * error * row['km']
 
 def readData(datafile):
 	for index, row in df.iterrows():
@@ -20,6 +33,7 @@ def main():
 		print("Usage: python3 train.py <datafile>")
 		sys.exit(1)
 	readData(sys.argv[1])
+	viewData()
 
 if __name__ == "__main__":
 	main()
